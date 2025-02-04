623a9db85d1e3757bab30c1cef277d3029f71f98
vSainiuk <vadik.sajnuykk@gmail.com>
2025-02-04 17:42:42 +0200

    chore:commented color picker page

--- src/app/a/color-picker/page.tsx
+++ src/app/a/color-picker/page.tsx
@@ -1,47 +1,49 @@
 'use client'
 
+import React from 'react'
+
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 
-const Chrome = dynamic(() => import('@uiw/react-color-chrome'), { ssr: false })
+// const Chrome = dynamic(() => import('@uiw/react-color-chrome'), { ssr: false })
 
 export default function ColorPickerPage() {
-	const [hsva, setHsva] = useState({ h: 0, s: 0, v: 100, a: 1 })
-	const [hex, setHex] = useState('#ffffff')
+	// const [hsva, setHsva] = useState({ h: 0, s: 0, v: 100, a: 1 })
+	// const [hex, setHex] = useState('#ffffff')
 
-	useEffect(() => {
-		import('@uiw/color-convert').then(({ hsvaToHex }) => {
-			setHex(hsvaToHex(hsva))
-		})
-	}, [hsva])
+	// useEffect(() => {
+	// 	import('@uiw/color-convert').then(({ hsvaToHex }) => {
+	// 		setHex(hsvaToHex(hsva))
+	// 	})
+	// }, [hsva])
 
-	return (
-		<div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-			<Chrome
-				color={hsva}
-				onChange={color => setHsva(color.hsva)}
-				style={{
-					marginBottom: '20px',
-					backgroundColor: '#201926',
-					padding: '10px',
-					borderRadius: '8px',
-				}}
-			/>
+	// return (
+	// 	<div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+	// 		<Chrome
+	// 			color={hsva}
+	// 			onChange={color => setHsva(color.hsva)}
+	// 			style={{
+	// 				marginBottom: '20px',
+	// 				backgroundColor: '#201926',
+	// 				padding: '10px',
+	// 				borderRadius: '8px',
+	// 			}}
+	// 		/>
 
-			<div
-				style={{
-					background: hex,
-					height: '100px',
-					width: '100px',
-					borderRadius: '8px',
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-					color: '#d63b3b',
-					fontWeight: 'bold',
-					boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-				}}
-			></div>
-		</div>
-	)
+	// 		<div
+	// 			style={{
+	// 				background: hex,
+	// 				height: '100px',
+	// 				width: '100px',
+	// 				borderRadius: '8px',
+	// 				display: 'flex',
+	// 				alignItems: 'center',
+	// 				justifyContent: 'center',
+	// 				color: '#d63b3b',
+	// 				fontWeight: 'bold',
+	// 				boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+	// 			}}
+	// 		></div>
+	// 	</div>
+	// )
 }
--- src/app/a/tasks-lite/page.tsx
+++ src/app/a/tasks-lite/page.tsx
@@ -2,12 +2,14 @@
 
 import { HEIGHT } from '@/constants/height-elements.constants'
 import { MarkdownProvider } from '@/contexts/MarkdownContext'
-import KanbanView from './kanban-view/KanbanView'
+import dynamic from 'next/dynamic'
+
+const KanbanView = dynamic(() => import('./kanban-view/KanbanView'), { ssr: false })
 
 export default function TasksPage() {
 	return (
 		<div
-		className='px-5'
+			className='px-5'
 			style={{
 				height: `calc(100% - ${HEIGHT.header})`,
 			}}
